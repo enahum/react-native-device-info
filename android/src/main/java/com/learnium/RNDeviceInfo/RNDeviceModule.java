667cfa683076c6ac3250e4a259df175e4e73b279
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-05-17 19:18:33 -0400

    Cache DeviceInfo constants

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -14,6 +14,7 @@ import android.content.res.Configuration;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 import android.os.Build;
+import android.os.SystemClock;
 import android.os.Environment;
 import android.os.StatFs;
 import android.os.BatteryManager;
@@ -42,6 +43,9 @@ import java.net.NetworkInterface;
 import javax.annotation.Nullable;
 
 public class RNDeviceModule extends ReactContextBaseJavaModule {
+
+    public static final String DEVICE_INFO_PREFERENCES = "DEVICE_INFO_PREFERENCES";
+    public static final Long CACHE_EXPIRATION = (long) 4 * 60 * 60 * 1000;
 
   ReactApplicationContext reactContext;
 
@@ -168,7 +172,7 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
       }
     }
 
-    p.resolve(macAddress);    
+    p.resolve(macAddress);
   }
 
   @ReactMethod
@@ -216,7 +220,14 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
   @Override
   public @Nullable
   Map<String, Object> getConstants() {
+    ReactApplicationContext appContext = this.getReactApplicationContext();
+    Context context = appContext.getApplicationContext();
+    SharedPreferences preferences = context.getSharedPreferences(DEVICE_INFO_PREFERENCES, Context.MODE_PRIVATE);
     HashMap<String, Object> constants = new HashMap<String, Object>();
+
+    if (preferences.getString("appName", null) != null) {
+      return this.getFromCache(preferences, context);
+    }
 
     PackageManager packageManager = this.reactContext.getPackageManager();
     String packageName = this.reactContext.getPackageName();
@@ -306,6 +317,215 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
     actMgr.getMemoryInfo(memInfo);
     constants.put("totalMemory", memInfo.totalMem);
 
+    this.saveConstants(constants, context);
+
     return constants;
+  }
+
+  private Map<String, Object> getFromCache(SharedPreferences preferences, Context context) {
+    Map<String, Object> constants = new HashMap<>();
+
+    Long cacheTime = preferences.getLong("cacheTime", 0);
+    Long timeNow = SystemClock.elapsedRealtime();
+
+    // recreate non-volatile constants from cache once expired
+    if (cacheTime != 0 && (timeNow - cacheTime) > CACHE_EXPIRATION) {
+      constants.putAll(this.recreateVolatileConstants(preferences));
+      constants.putAll(this.addNonVolatileConstants(preferences));
+      constants.put("cacheTime", timeNow);
+      this.saveConstants(constants, context);
+
+      return constants;
+    }
+
+    constants.put("appVersion", preferences.getString("appVersion", ""));
+    constants.put("appName", preferences.getString("appName", ""));
+    constants.put("buildVersion", preferences.getString("buildVersion", ""));
+
+    constants.put("buildNumber", preferences.getInt("buildNumber", 0));
+    constants.put("firstInstallTime", preferences.getLong("firstInstallTime", 0));
+    constants.put("lastUpdateTime", preferences.getLong("lastUpdateTime", 0));
+
+    constants.put("instanceId", preferences.getString("instanceId", ""));
+    constants.put("serialNumber", preferences.getString("serialNumber", ""));
+    constants.put("deviceName", preferences.getString("deviceName", ""));
+    constants.put("systemName", preferences.getString("systemName", ""));
+    constants.put("model", preferences.getString("model", ""));
+    constants.put("brand", preferences.getString("brand", ""));
+    constants.put("deviceId", preferences.getString("deviceId", ""));
+    constants.put("apiLevel", preferences.getString("apiLevel", ""));
+    constants.put("deviceLocale", preferences.getString("deviceLocale", ""));
+    constants.put("deviceCountry", preferences.getString("deviceCountry", ""));
+    constants.put("uniqueId", preferences.getString("uniqueId", ""));
+    constants.put("systemManufacturer", preferences.getString("systemManufacturer", ""));
+    constants.put("bundleId", preferences.getString("bundleId", ""));
+    constants.put("userAgent", preferences.getString("userAgent", ""));
+    constants.put("timezone", preferences.getString("timezone", ""));
+
+    constants.put("isEmulator", preferences.getBoolean("isEmulator", false));
+    constants.put("isTablet", preferences.getBoolean("isTablet", false));
+    constants.put("is24Hour", preferences.getBoolean("is24Hour", false));
+
+    constants.put("phoneNumber", preferences.getString("phoneNumber", ""));
+    constants.put("carrier", preferences.getString("carrier", ""));
+    constants.put("maxMemory", preferences.getLong("maxMemory", 0));
+    constants.put("totalMemory", preferences.getLong("totalMemory", 0));
+
+    return constants;
+  }
+
+  /**
+   * volatile data
+   * - appVersion
+   * - buildNumber
+   * <p>
+   * - firstInstallTime
+   * - lastUpdateTime
+   * - apiLevel
+   * <p>
+   * - deviceLocale
+   * - deviceCountry
+   * - timezone
+   * - is24Hour
+   */
+  private Map<String, Object> recreateVolatileConstants(SharedPreferences preferences) {
+    Map<String, Object> constants = new HashMap<>();
+    try {
+      String packageName = this.reactContext.getPackageName();
+      PackageManager packageManager = this.reactContext.getPackageManager();
+      PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+
+      // volatile data
+      constants.put("appVersion", info.versionName);
+      constants.put("buildNumber", info.versionCode);
+
+      constants.put("firstInstallTime", info.firstInstallTime);
+      constants.put("lastUpdateTime", info.lastUpdateTime);
+      constants.put("apiLevel", Build.VERSION.SDK_INT);
+
+      constants.put("deviceLocale", this.getCurrentLanguage());
+      constants.put("deviceCountry", this.getCurrentCountry());
+      constants.put("timezone", TimeZone.getDefault().getID());
+      constants.put("is24Hour", this.is24Hour());
+    } catch (PackageManager.NameNotFoundException exception) {
+      constants.put("appVersion", preferences.getString("appVersion", ""));
+      constants.put("buildNumber", preferences.getInt("buildNumber", 0));
+
+      constants.put("firstInstallTime", preferences.getLong("firstInstallTime", 0));
+      constants.put("lastUpdateTime", preferences.getLong("lastUpdateTime", 0));
+      constants.put("apiLevel", preferences.getString("apiLevel", ""));
+
+      constants.put("deviceLocale", preferences.getString("deviceLocale", ""));
+      constants.put("deviceCountry", preferences.getString("deviceCountry", ""));
+      constants.put("timezone", preferences.getString("timezone", ""));
+      constants.put("is24Hour", preferences.getBoolean("is24Hour", false));
+    }
+
+    return constants;
+  }
+
+  /**
+   * non-volatile data
+   * - appName
+   * - buildVersion
+   *
+   * - instanceId
+   * - serialNumber
+   * - deviceName
+   * - systemName
+   * - model
+   * - brand
+   * - deviceId
+   *
+   * - uniqueId
+   * - systemManufacturer
+   * - bundleId
+   * - userAgent
+   *
+   * - isEmulator
+   * - isTablet
+   *
+   * - phoneNumber
+   * - carrier
+   * - maxMemory
+   * - totalMemory
+   */
+  private Map<String, Object> addNonVolatileConstants(SharedPreferences preferences) {
+    Map<String, Object> constants = new HashMap<>();
+
+    constants.put("appName", preferences.getString("appName", ""));
+    constants.put("buildVersion", preferences.getString("buildVersion", ""));
+
+    constants.put("instanceId", preferences.getString("instanceId", ""));
+    constants.put("serialNumber", preferences.getString("serialNumber", ""));
+    constants.put("deviceName", preferences.getString("deviceName", ""));
+    constants.put("systemName", preferences.getString("systemName", ""));
+    constants.put("model", preferences.getString("model", ""));
+    constants.put("brand", preferences.getString("brand", ""));
+    constants.put("deviceId", preferences.getString("deviceId", ""));
+
+    constants.put("uniqueId", preferences.getString("uniqueId", ""));
+    constants.put("systemManufacturer", preferences.getString("systemManufacturer", ""));
+    constants.put("bundleId", preferences.getString("bundleId", ""));
+    constants.put("userAgent", preferences.getString("userAgent", ""));
+
+    constants.put("isEmulator", preferences.getBoolean("isEmulator", false));
+    constants.put("isTablet", preferences.getBoolean("isTablet", false));
+
+    constants.put("phoneNumber", preferences.getString("phoneNumber", ""));
+    constants.put("carrier", preferences.getString("carrier", ""));
+    constants.put("maxMemory", preferences.getLong("maxMemory", 0));
+    constants.put("totalMemory", preferences.getLong("totalMemory", 0));
+
+    return constants;
+  }
+
+  private void saveConstants(Map<String, Object> constants, Context ctx) {
+    SharedPreferences preferences = ctx.getSharedPreferences(DEVICE_INFO_PREFERENCES, Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = preferences.edit();
+
+    // volatile data
+    editor.putString("appVersion", constants.get("appVersion").toString());
+    editor.putInt("buildNumber", (Integer) constants.get("buildNumber"));
+    editor.putLong("firstInstallTime", (Long) constants.get("firstInstallTime"));
+    editor.putLong("lastUpdateTime", (Long) constants.get("lastUpdateTime"));
+
+    editor.putString("apiLevel", constants.get("apiLevel").toString());
+    editor.putString("deviceLocale", constants.get("deviceLocale").toString());
+    editor.putString("deviceCountry", constants.get("deviceCountry").toString());
+    editor.putString("timezone", constants.get("timezone").toString());
+
+    editor.putBoolean("is24Hour", (Boolean) constants.get("is24Hour"));
+
+    // non-volatile data
+    editor.putString("appName", constants.get("appName").toString());
+    editor.putString("buildVersion", constants.get("buildVersion").toString());
+
+    editor.putString("instanceId", constants.get("instanceId").toString());
+    editor.putString("serialNumber", constants.get("serialNumber").toString());
+    editor.putString("deviceName", constants.get("deviceName").toString());
+    editor.putString("systemName", constants.get("systemName").toString());
+    editor.putString("model", constants.get("model").toString());
+    editor.putString("brand", constants.get("brand").toString());
+    editor.putString("deviceId", constants.get("deviceId").toString());
+    editor.putString("uniqueId", constants.get("uniqueId").toString());
+    editor.putString("systemManufacturer", constants.get("systemManufacturer").toString());
+    editor.putString("bundleId", constants.get("bundleId").toString());
+    editor.putString("userAgent", constants.get("userAgent").toString());
+
+    editor.putBoolean("isEmulator", (Boolean) constants.get("isEmulator"));
+    editor.putBoolean("isTablet", (Boolean) constants.get("isTablet"));
+
+
+    if (constants.get("phoneNumber") != null) {
+      editor.putString("phoneNumber", constants.get("phoneNumber").toString());
+    }
+    editor.putString("carrier", constants.get("carrier").toString());
+    editor.putLong("maxMemory", (Long) constants.get("maxMemory"));
+    editor.putLong("totalMemory", (Long) constants.get("totalMemory"));
+
+    editor.putLong("cacheTime", SystemClock.elapsedRealtime());
+
+    editor.commit();
   }
 }
